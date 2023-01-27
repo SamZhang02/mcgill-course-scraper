7b695e21c9bff90b48b9d579426c4a0e58c6a561
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-01-26 20:47:07 -0500

    can now scrape overview and catalog notes

--- src/scraper.py
+++ src/scraper.py
@@ -20,11 +20,13 @@ def get_course_credits(title:list) -> int:
 def get_course_name(title:list) ->str:
     return " ".join(title[2:-2])
 
+def get_faculty(html:bs.BeautifulSoup) -> str:
+    string = html.find_all("div",class_='meta')[0]
+    return string.getText().strip().removeprefix("Offered by: ")
+
 def get_terms(html:bs.BeautifulSoup) -> list:
-    string = str(html.find_all("p",class_="catalog-terms")[0])
-    string = string.removeprefix("<p class=\"catalog-terms\">")
-    string = string.removesuffix("</p>")
-    string = string.strip()
+    string = html.find_all("p",class_="catalog-terms")[0]
+    string = string.getText().strip()
     string = string.removeprefix("Terms:")
     output = string.strip().split(",")
     if "not" in output[0]:
@@ -32,10 +34,8 @@ def get_terms(html:bs.BeautifulSoup) -> list:
     return output 
 
 def get_profs(html:bs.BeautifulSoup) -> list:
-    string = str(html.find_all("p",class_="catalog-instructors")[0])
-    string = string.removeprefix("<p class=\"catalog-instructors\">")
-    string = string.removesuffix("</p>")
-    string = string.strip()
+    string = html.find_all("p",class_="catalog-instructors")[0]
+    string = string.getText().strip()
     string = string.removeprefix("Instructors:")
     string = string.removesuffix("(Winter)")
     output = string.strip().split("(Fall)")
@@ -45,12 +45,27 @@ def get_profs(html:bs.BeautifulSoup) -> list:
         output[i] = [x.strip() for x in output[i].split(";")]
     return output
 
+def get_overview(html:bs.BeautifulSoup) -> str:
+    string = html.select("#node-22180 > div.content > p:nth-child(2)")[0]
+    output = string.getText().strip()
+    return output
+
+def get_notes(html:bs.BeautifulSoup) -> list:
+    string = html.find_all("ul","catalog-notes")[0]
+    output = string.getText()
+    output = output.strip().split("\n\n")
+    return output
+
+
 if __name__ == "__main__":
-    course = get_html_soup("https://www.mcgill.ca/study/2022-2023/courses/math-240")
+    course = get_html_soup("https://www.mcgill.ca/study/2022-2023/courses/mgcr-271")
     title = get_title_words(course)
     print(get_course_code(title))
     print(get_course_credits(title))
     print(get_course_name(title))
+    print(get_faculty(course))
     print(get_terms(course))
     print(get_profs(course))
+    print(get_overview(course))
+    print(get_notes(course))
     pass
